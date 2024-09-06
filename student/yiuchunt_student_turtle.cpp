aa55fd557494781fe7e9a13522f7527dac473bd5
perrintong <perrintong@icloud.com>
2024-09-06 17:17:34 -0400

    added default cases

--- student/yiuchunt_student_turtle.cpp
+++ student/yiuchunt_student_turtle.cpp
@@ -19,16 +19,13 @@ turtleMove studentTurtleStep(bool bumped) { return MOVE; }
 // OK TO MODIFY BELOW THIS LINE
 
 #define TIMEOUT                                                                \
-  40 // bigger number slows down simulation so you can see what's happening
+  4 // bigger number slows down simulation so you can see what's happening
 float cycle, current_state, prev_state;
 float fx1, fy1, fx2, fy2;
 float solved, bump;
 
 enum direction { LEFT = 0, DOWN = 1, RIGHT = 2, UP = 3 };
-enum state {
-  GO = 1,
-  CHECKBP = 0
-};
+enum state { GO = 1, CHECKBP = 0 };
 
 // this procedure takes the current turtle position and orientation and returns
 // true=submit changes, false=do not submit changes
@@ -43,64 +40,73 @@ bool studentMoveTurtle(QPointF &pos_, int &new_or) {
     fx2 = pos_.x();
     fy2 = pos_.y();
 
-    switch (new_or) { 
-    case LEFT:
-      fy2 += 1;
-      break;
-    case DOWN:
-      fx2 += 1;
-      break;
-    case RIGHT:
-      fy2 += 1;
-      break;
-    case UP:
-      fx2 += 1;
-      break;
+    switch (new_or) {
+      case LEFT:
+        fy2 += 1;
+        break;
+      case DOWN:
+        fx2 += 1;
+        break;
+      case RIGHT:
+        fx2 += 1;
+        fy2 += 1;
+        fx1 += 1;
+        break;
+      case UP:
+        fx2 += 1;
+        fy2 += 1;
+        fy1 += 1;
+        break;
+      default:
+        break;
     }
 
     bump = bumped(fx1, fy1, fx2, fy2);
     solved = atend(pos_.x(), pos_.y());
 
     // if went straight last cycle turn right to find new path
-    if (prev_state == GO) {        
+    if (prev_state == GO) {
       switch (new_or) {
-      case LEFT:
-        new_or = DOWN;
-        break;
-      case DOWN:
-        new_or = RIGHT;
-        break;
-      case RIGHT:
-        new_or = UP;
-        break;
-      case UP:
-        new_or = LEFT;
-        break;
-      }
-      current_state = CHECKBP;
-    } else {           // previous state here is checkbp
-      if (bump) {      // if bumped after turn undo turn/turn left 
-        switch (new_or) {
         case LEFT:
-          new_or = UP;
-          break;
-        case DOWN:
-          new_or = LEFT;
-          break;
-        case RIGHT:
           new_or = DOWN;
           break;
-        case UP:
+        case DOWN:
           new_or = RIGHT;
           break;
+        case RIGHT:
+          new_or = UP;
+          break;
+        case UP:
+          new_or = LEFT;
+          break;
+        default:
+          break;
+      }
+      current_state = CHECKBP;
+    } else {      // previous state here is checkbp
+      if (bump) { // if bumped after turn undo turn/turn left
+        switch (new_or) {
+          case LEFT:
+            new_or = UP;
+            break;
+          case DOWN:
+            new_or = LEFT;
+            break;
+          case RIGHT:
+            new_or = DOWN;
+            break;
+          case UP:
+            new_or = RIGHT;
+            break;
+          default:
+            break;
         }
-      } else {      // if there's no bump, go straight
+      } else { // if there's no bump, go straight
         current_state = GO;
       }
     }
-    
+
     ROS_INFO("Orientation=%f  STATE=%f", new_or, current_state);
-    prev_state = current_state;
 
     if (current_state == GO && solved == false) {
       if (new_or == DOWN)
@@ -112,8 +118,10 @@ bool studentMoveTurtle(QPointF &pos_, int &new_or) {
       if (new_or == LEFT)
         pos_.setX(pos_.x() - 1);
     }
+
+    prev_state = current_state;
   }
-  
+
   if (solved)
     return false;
 
