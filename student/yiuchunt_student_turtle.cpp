ca5f63799aad92848dd07a44ae9e4418d6e50850
perrintong <perrintong@icloud.com>
2024-09-12 18:38:19 -0400

    removed define and replaced float with int

--- student/yiuchunt_student_turtle.cpp
+++ student/yiuchunt_student_turtle.cpp
@@ -18,11 +18,11 @@ turtleMove studentTurtleStep(bool bumped) { return MOVE; }
 
 // OK TO MODIFY BELOW THIS LINE
 
-#define TIMEOUT                                                                \
-  4 // bigger number slows down simulation so you can see what's happening
-float cycle, current_state, prev_state;
-float fx1, fy1, fx2, fy2;
-float solved, bump;
+const int TIMEOUT =
+    4; // bigger number slows down simulation so you can see what's happening
+int cycle, current_state, prev_state;
+int fx1, fy1, fx2, fy2;
+int solved, bump;
 
 enum direction { LEFT = 0, DOWN = 1, RIGHT = 2, UP = 3 };
 enum state { GO = 1, CHECKBP = 0 };
@@ -41,24 +41,24 @@ bool studentMoveTurtle(QPointF &pos_, int &new_or) {
     fy2 = pos_.y();
 
     switch (new_or) {
-      case LEFT:
-        fy2 += 1;
-        break;
-      case DOWN:
-        fx2 += 1;
-        break;
-      case RIGHT:
-        fx2 += 1;
-        fy2 += 1;
-        fx1 += 1;
-        break;
-      case UP:
-        fx2 += 1;
-        fy2 += 1;
-        fy1 += 1;
-        break;
-      default:
-        break;
+    case LEFT:
+      fy2 += 1;
+      break;
+    case DOWN:
+      fx2 += 1;
+      break;
+    case RIGHT:
+      fx2 += 1;
+      fy2 += 1;
+      fx1 += 1;
+      break;
+    case UP:
+      fx2 += 1;
+      fy2 += 1;
+      fy1 += 1;
+      break;
+    default:
+      break;
     }
 
     bump = bumped(fx1, fy1, fx2, fy2);
@@ -67,39 +67,39 @@ bool studentMoveTurtle(QPointF &pos_, int &new_or) {
     // if went straight last cycle turn right to find new path
     if (prev_state == GO) {
       switch (new_or) {
-        case LEFT:
-          new_or = DOWN;
-          break;
-        case DOWN:
-          new_or = RIGHT;
-          break;
-        case RIGHT:
-          new_or = UP;
-          break;
-        case UP:
-          new_or = LEFT;
-          break;
-        default:
-          break;
+      case LEFT:
+        new_or = DOWN;
+        break;
+      case DOWN:
+        new_or = RIGHT;
+        break;
+      case RIGHT:
+        new_or = UP;
+        break;
+      case UP:
+        new_or = LEFT;
+        break;
+      default:
+        break;
       }
       current_state = CHECKBP;
     } else {      // previous state here is checkbp
       if (bump) { // if bumped after turn undo turn/turn left
         switch (new_or) {
-          case LEFT:
-            new_or = UP;
-            break;
-          case DOWN:
-            new_or = LEFT;
-            break;
-          case RIGHT:
-            new_or = DOWN;
-            break;
-          case UP:
-            new_or = RIGHT;
-            break;
-          default:
-            break;
+        case LEFT:
+          new_or = UP;
+          break;
+        case DOWN:
+          new_or = LEFT;
+          break;
+        case RIGHT:
+          new_or = DOWN;
+          break;
+        case UP:
+          new_or = RIGHT;
+          break;
+        default:
+          break;
         }
       } else { // if there's no bump, go straight
         current_state = GO;
