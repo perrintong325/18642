5b108947f917b02577e3da9d6a803d00582c2671
perrintong <perrintong@icloud.com>
2024-10-31 13:31:31 -0400

    run and build shell script

---
+++ yiuchunt_build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Will kill all processes that are passed as arguments
+# Used with trap to guarantee that CTRL+C on this script kills all BG processes
+kill_processes() {
+    for p in "$@"; do
+	if [[ -z $(ps -p $p > /dev/null) ]]; then
+	    echo "Killing process $p"
+	    kill $p
+	    sleep 1
+	fi
+    done
+    echo "killed all processes, exiting"
+    exit 0
+}
+
+# Arguments checking
+maze_file=""
+if [[ $# -gt 0 ]]; then
+    echo "No arguments allowed: ./yiuchunt_build.sh"
+    exit 1
+fi
+
+target_directory="catkin_ws/src"
+
+if [ -d "$target_directory/ece642rtle" ]; then
+    echo "Removing existing ece642rtle directory"
+    rm -rf "$target_directory/ece642rtle"
+fi
+
+tar -zxvf ANDREWID_files.tgz -C "$target_directory"
+
+# Locate the ece642rtle directory
+turtledir=`rospack find ece642rtle`
+if [ -z "$turtledir" ]; then
+    echo "Cannot locate package ece642rtle."
+    echo "Did you run catkin_make (with no arguments) in the"
+    echo " Catkin workspace before proceeding?"
+    exit 1
+fi
+
+# Catkin WS is two directories above package directory
+cd "$turtledir/../.."
+echo "Working from catkin workspace $(pwd)"
+echo ""
+
+# Build the student node
+catkin_make ece642rtle_student
+if [ $? -ne 0 ]; then
+    echo "catkin_make did not succeed, exiting script"
+    exit 1
+fi
+
+# So ROS can find everything
+source devel/setup.bash
+
+cd ~
+
+echo "Build successful"
---
+++ yiuchunt_run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Will kill all processes that are passed as arguments
+# Used with trap to guarantee that CTRL+C on this script kills all BG processes
+kill_processes() {
+    for p in "$@"; do
+	if [[ -z $(ps -p $p > /dev/null) ]]; then
+	    echo "Killing process $p"
+	    kill $p
+	    sleep 1
+	fi
+    done
+    echo "killed all processes, exiting"
+    exit 0
+}
+
+# Arguments checking
+maze_number=$1
+maze_file="m${maze_number}.maze"
+if [[ $# -gt 1 ]]; then
+    echo "Only one argument allowed: yiuchunt_run.sh [-h|--help] [MAZEFILE_NAME]"
+    exit 1
+fi
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "Usage: ./yiuchunt_run.sh [-h|--help] [MAZEFILE_NAME]"
+    exit 2
+fi
+
+# Run roscore if it is not already running
+ROSCORE_PID=""
+if [[ -z $(pgrep roscore) ]]; then
+    roscore&
+    ROSCORE_PID=$!
+    sleep 1
+    if [[ -z $(pgrep roscore) ]]; then
+	echo "Error launching roscore. Make sure no other ros processes are running and try again."
+	exit 1
+    fi
+fi
+# Have to kill BG process if user exits
+trap 'kill_processes $ROSCORE_PID' SIGINT
+sleep 5
+
+# If maze file argument is provided, pass it to the backend via ros param
+# (code backend defaults to m1.maze so we don't worry about that case)
+if [[ -n $maze_file ]]; then
+    rosparam set /maze_file "$maze_file"
+fi
+
+# Node that displays the maze and runs the turtle
+rosrun ece642rtle ece642rtle_node&
+TURTLE_PID=$!
+sleep 1
+if [[ -z $(pgrep ece642rtle_node) ]]; then
+    echo "Error launching ece642rtle_node. Have you run catkin_make?"
+    kill_processes $ROSCORE_PID
+    exit 1
+fi
+# Have to kill BG processes if user exits
+trap 'kill_processes $TURTLE_PID $ROSCORE_PID' SIGINT
+sleep 9
+
+# Student node
+rosrun ece642rtle ece642rtle_student&
+STUDENT_PID=$!
+# Have to kill BG processes if user exits
+trap 'kill_processes $STUDENT_PID $TURTLE_PID $ROSCORE_PID' SIGINT
+
+# Spin
+while [ 1 -eq 1 ]; do
+    sleep 30
+done
+
